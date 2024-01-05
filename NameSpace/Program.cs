dd996717d1dcc29f7023d7a57380a9b702f4f048
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-01-05 12:39:40 +0300

    namespace kullanımı

---
+++ NameSpace/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace NameSpace
+{
+    namespace AnotherNameSpace
+    {
+        public class MyDenemeClass
+        {
+            public int DenemeVeri { get; set; }
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+
+    //Class
+    public class MyClass
+    {
+        public MyClass()
+        {
+            Veri = 10;
+        }
+
+        public int Veri { get;}
+    }
+    
+    //Struct
+    public struct MyStruct
+    {
+        
+    }
+    //Interface
+    public interface IMyInterface
+    {
+        
+    }
+    //Enum
+    public enum MyEnum{
+    
+    }
+    
+    //Delegate
+    /// <summary>
+    /// Remarks
+    /// </summary>
+    public delegate void MyDelegate(string a);
+}
+
+
+namespace System
+{
+    namespace System.Reflection
+    {
+        namespace System.Reflection.Metadata
+        {
+            namespace System.Reflection.Metadata.Ecma335
+            {
+                
+            }
+        }
+    }
+}
+
---
+++ UsingNameSpace/Program.cs
@@ -0,0 +1,10 @@
+﻿//Bu şekilde kullanılırsa proje içinde tekrar tekrar belirtmek zorunda kalmayız
+using NameSpace;
+using NameSpace.AnotherNameSpace;
+
+//Bu şekilde kullanılırsa hangi namespace içinde olduğunu yazıp noktayla istenilen yapıya ulaşırız.
+NameSpace.MyClass myClass = new();
+NameSpace.AnotherNameSpace.MyDenemeClass myDenemeClass = new NameSpace.AnotherNameSpace.MyDenemeClass();
+
+
+Console.WriteLine("Hello, World!");
