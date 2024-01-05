5cac52a89c9d06d242a9a0e3698b8ec31cffcfb7
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-01-05 13:54:09 +0300

    Using Assembly

---
+++ Reflection/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+Assembly assembly = typeof(Example).Assembly;
+
+Console.WriteLine(assembly.FullName);
+AssemblyName assemblyName = assembly.GetName();
+
+
+public class Example
+{
+    private int factor;
+
+    public Example(int fac)
+    {
+        factor = fac;
+    }
+
+    public int SampleMethod(int x)
+    {
+        Console.WriteLine("\nExample.SampleMethod({0}) executes.", x);
+        return x * factor;
+    }
+}
