89c32888a6be055c299272490eb72f3d648033c2
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-01-17 13:19:30 +0300

    Virtual

---
+++ VirtualKeyword/Program.cs
@@ -0,0 +1,50 @@
+﻿DerivedClass derivedClass = new();
+derivedClass.Print("Hello");
+derivedClass.PrintVirtual("Hello");
+
+
+
+public class BaseClass
+{
+    public void Print(string a)
+    {
+        Console.WriteLine($"{a}");
+    }
+
+    public virtual void PrintVirtual(string a)
+    {
+        Console.WriteLine($"{a}");
+    }
+}
+
+public class DerivedClass : BaseClass
+{
+    public override void PrintVirtual(string a)
+    {
+        Console.WriteLine($"DerivedClass Method {a}");
+    }
+}
+
+public abstract class MyAbstractClass
+{
+    public void MyMethod()
+    {
+        
+    }
+
+    public abstract void MyAbstractMethod();
+}
+
+public class MyClass : MyAbstractClass
+{
+    //Sadece abstract olan member override edildi.
+    //Virtual keyword ile karıştırılmasın abstract memberlar sadece abstract classlar içinde yazırlır ve 
+    //abstract olan memberlar hiç bir şekilde base classta doldurulamaz
+    //Ancak bir virtual member base classta da doldurulup istenirse eğer derived classta da doldurulabilir.
+    //Tabi ki derived class kullanılıyorsa ve virtual member ezilip yeni hali doldurulmuşsa, yeni hali kullanılır.
+    public override void MyAbstractMethod()
+    {
+        throw new NotImplementedException();
+    }
+    
+}
--- VirtualKeyword/obj/Debug/net8.0/VirtualKeyword.AssemblyInfo.cs
+++ VirtualKeyword/obj/Debug/net8.0/VirtualKeyword.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Events")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("VirtualKeyword")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30a99c6a75974f3c6cd152eeacfa8bde0b5c24a4")]
-[assembly: System.Reflection.AssemblyProductAttribute("Events")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Events")]
+[assembly: System.Reflection.AssemblyProductAttribute("VirtualKeyword")]
+[assembly: System.Reflection.AssemblyTitleAttribute("VirtualKeyword")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
